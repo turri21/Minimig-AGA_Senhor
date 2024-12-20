4db1bf2a530d8cfcd00689213645c8d00d9c562d
turri21 <154733583+turri21@users.noreply.github.com>
2024-12-20 21:49:49 +0200

    Add files via upload

--- sys/sys.tcl
+++ sys/sys.tcl
@@ -6,18 +6,6 @@ set_global_assignment -name DEVICE_FILTER_SPEED_GRADE 7
 set_global_assignment -name RESERVE_ALL_UNUSED_PINS_WEAK_PULLUP "AS OUTPUT DRIVING GROUND"
 
 #============================================================
-# ADC
-#============================================================
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to ADC_CONVST
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to ADC_SCK
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to ADC_SDI
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to ADC_SDO
-set_location_assignment PIN_U9 -to ADC_CONVST
-set_location_assignment PIN_V10 -to ADC_SCK
-set_location_assignment PIN_AC4 -to ADC_SDI
-set_location_assignment PIN_AD4 -to ADC_SDO
- 
-#============================================================
 # CLOCK
 #============================================================
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK1_50
@@ -27,26 +15,6 @@ set_location_assignment PIN_V11 -to FPGA_CLK1_50
 set_location_assignment PIN_Y13 -to FPGA_CLK2_50
 set_location_assignment PIN_E11 -to FPGA_CLK3_50
 
-#============================================================
-# SPI SD
-#============================================================
-set_location_assignment PIN_AE15 -to SD_SPI_CS
-#set_location_assignment PIN_AH8  -to SD_SPI_MISO
-#set_location_assignment PIN_AG8  -to SD_SPI_CLK
-set_location_assignment PIN_U13  -to SD_SPI_MOSI
-set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to SD_SPI*
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SD_SPI*
-set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to SD_SPI*
-
-#============================================================
-# I2C LEDS/BUTTONS
-#============================================================
-set_location_assignment PIN_U14 -to IO_SCL
-#set_location_assignment PIN_AG9 -to IO_SDA
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to IO_S*
-set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to IO_S*
-set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to IO_S*
- 
 #============================================================
 # SDRAM
 #============================================================
@@ -79,23 +47,25 @@ set_location_assignment PIN_AG21 -to SDRAM_DQ[10]
 set_location_assignment PIN_AH21 -to SDRAM_DQ[11]
 set_location_assignment PIN_AH23 -to SDRAM_DQ[12]
 set_location_assignment PIN_AH22 -to SDRAM_DQ[13]
-
 set_location_assignment PIN_AH24 -to SDRAM_DQ[14]
 set_location_assignment PIN_AG23 -to SDRAM_DQ[15]
-
 set_location_assignment PIN_AH19 -to SDRAM_CLK
 set_location_assignment PIN_AG14 -to SDRAM_nWE
 set_location_assignment PIN_AH12 -to SDRAM_nCAS
 set_location_assignment PIN_AG11 -to SDRAM_nCS
 set_location_assignment PIN_AG13 -to SDRAM_nRAS
- 
+
+set_instance_assignment -name VIRTUAL_PIN ON -to SDRAM_DQML
+set_instance_assignment -name VIRTUAL_PIN ON -to SDRAM_DQMH
+set_instance_assignment -name VIRTUAL_PIN ON -to SDRAM_CKE
+
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to SDRAM_*
 set_instance_assignment -name CURRENT_STRENGTH_NEW "MAXIMUM CURRENT" -to SDRAM_*
 set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to SDRAM_*
 set_instance_assignment -name FAST_OUTPUT_ENABLE_REGISTER ON -to SDRAM_DQ[*]
 set_instance_assignment -name FAST_INPUT_REGISTER ON -to SDRAM_DQ[*]
 set_instance_assignment -name ALLOW_SYNCH_CTRL_USAGE OFF -to *|SDRAM_*
- 
+
 #============================================================
 # HDMI
 #============================================================
@@ -132,12 +102,8 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[21]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[22]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_D[23]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_HS
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_INT
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to HDMI_TX_VS
-#set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_D[*]
-#set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_DE
-#set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_HS
-#set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_VS
-#set_instance_assignment -name FAST_OUTPUT_REGISTER ON -to HDMI_TX_CLK
 set_location_assignment PIN_U10 -to HDMI_I2C_SCL
 set_location_assignment PIN_AA4 -to HDMI_I2C_SDA
 set_location_assignment PIN_T13 -to HDMI_I2S
@@ -171,8 +137,9 @@ set_location_assignment PIN_AG6 -to HDMI_TX_D[21]
 set_location_assignment PIN_AF9 -to HDMI_TX_D[22]
 set_location_assignment PIN_AE8 -to HDMI_TX_D[23]
 set_location_assignment PIN_T8 -to HDMI_TX_HS
+set_location_assignment PIN_AF11 -to HDMI_TX_INT
 set_location_assignment PIN_V13 -to HDMI_TX_VS
- 
+
 #============================================================
 # KEY
 #============================================================
@@ -180,7 +147,7 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to KEY[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to KEY[1]
 set_location_assignment PIN_AH17 -to KEY[0]
 set_location_assignment PIN_AH16 -to KEY[1]
- 
+
 #============================================================
 # LED
 #============================================================
@@ -200,7 +167,7 @@ set_location_assignment PIN_AF26 -to LED[4]
 set_location_assignment PIN_AE26 -to LED[5]
 set_location_assignment PIN_Y16 -to LED[6]
 set_location_assignment PIN_AA23 -to LED[7]
- 
+
 #============================================================
 # SW
 #============================================================
@@ -212,12 +179,13 @@ set_location_assignment PIN_Y24 -to SW[0]
 set_location_assignment PIN_W24 -to SW[1]
 set_location_assignment PIN_W21 -to SW[2]
 set_location_assignment PIN_W20 -to SW[3]
- 
+
 set_instance_assignment -name HPS_LOCATION HPSINTERFACEPERIPHERALSPIMASTER_X52_Y72_N111 -entity sys_top -to spi
 set_instance_assignment -name HPS_LOCATION HPSINTERFACEPERIPHERALUART_X52_Y67_N111 -entity sys_top -to uart
 set_instance_assignment -name HPS_LOCATION HPSINTERFACEPERIPHERALI2C_X52_Y60_N111 -entity sys_top -to hdmi_i2c
- 
+
 set_global_assignment -name PRE_FLOW_SCRIPT_FILE "quartus_sh:sys/build_id.tcl"
- 
+
 set_global_assignment -name CDF_FILE jtag.cdf
 set_global_assignment -name QIP_FILE sys/sys.qip
+
