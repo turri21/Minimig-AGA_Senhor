b3f582937c807870262bdc9ac29232be7e98883e
thera34 <99019177+thera34@users.noreply.github.com>
2025-04-29 08:21:54 +0300

    Add 230400 baud option for PPP
    Added 230400 baud option for PPP

--- Minimig.sv
+++ Minimig.sv
@@ -167,7 +167,7 @@ assign HDMI_FREEZE = 0;
 
 `include "build_id.v" 
 localparam CONF_STR = {
-	"Minimig;UART115200,MIDI;",
+	"Minimig;UART115200:230400,MIDI;",
 	"J,Red(Fire),Blue,Yellow,Green,RT,LT,Pause;",
 	"jn,A,B,X,Y,R,L,Start;",
 	"jp,B,A,X,Y,R,L,Start;",
